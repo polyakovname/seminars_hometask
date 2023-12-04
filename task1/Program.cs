d2921069302918e47ca5eeb87f835e3565bbb9fb
denis <denis.polyakov02@yandex.ru>
2023-12-04 15:46:56 +0300

    first

---
+++ task1/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = new int[10];
+int result = 0;
+for (int i=0; i<array.Length; i++)
+{
+    array[i] = new Random().Next(1, 101);
+    Console.Write(array[i]+" ");
+}
+Console.WriteLine();
+for (int i=0; i<array.Length; i++)
+{
+    if (array[i]>=20 && array[i]<=90)
+    {
+        result++;
+    }
+}
+Console.Write(result);
